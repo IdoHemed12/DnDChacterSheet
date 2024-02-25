d9046dc6ab121942fb723913be2f790c4f0bfd06
Ido Hemed <IdoHemed12@gmail.com>
2024-02-25 19:56:52 +0200

    Fix+Added/Main/

--- Main.py
+++ Main.py
@@ -4,4 +4,9 @@ with open("Attributes.toml", 'r') as file:
     ConfigData = toml.load(file)
     
 def Character_Toml_Data(): 
-    PlayerName = ConfigData['PlayerName']
+    #Translating the configuration data So it could be refrenced later as a Variable 
+    PlayerName = ConfigData['CharacterName']
+    PlayerLevel = ConfigData['Level']
+    PlayerClass = ConfigData['Class']
+    PlayerRace = ConfigData['Race']
+Character_Toml_Data()
